233c92946ecc0fb748d2ef0ce75890a1f42cd373
Renan Araújo <renan.alvs@gmail.com>
2020-03-04 16:57:05 -0300

    translate.js created

---
+++ src/translate.js
@@ -0,0 +1,10 @@
+const defaultLang = 'pt';
+const supportedLang = [defaultLang, 'en', 'fr'];
+
+const translations = {
+    top: {
+        en: "PayPal is your digital wallet",
+        pt: "PayPal é sua carteira digital",
+        fr: "PayPal est votre portefeuille numérique"
+    }
+}
